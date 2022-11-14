b58a8aedf8b914347aa074620a4dd519c9799708
coji <coji@techtalk.jp>
2022-11-14 11:08:16 +0900

    init

---
+++ index.js
@@ -0,0 +1,56 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint", "testing-library", "unused-imports"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  rules: {
+    "vue/no-v-html": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/attribute-hyphenation": "off",
+    "lines-between-class-members": "off",
+    "func-names": "error",
+    "func-style": ["error", "expression", { allowArrowFunctions: true }],
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/array-type": [
+      "error",
+      {
+        default: "array-simple",
+      },
+    ],
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-floating-promises": "error",
+    "import/no-unresolved": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "testing-library/await-async-query": "error",
+    "testing-library/no-await-sync-query": "error",
+    "testing-library/no-debugging-utils": "warn",
+    semi: ["warn", "never"],
+  },
+};
